6bd7cfbf20e4d3994aa2a318dce35fb554d3e301
Nexjohan <167144153+Nexjohan@users.noreply.github.com>
2024-06-26 17:41:47 -0500

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/*.{html,js,php}","./public/js/*.{html,js}"], //llamando los estilos de la carpeta js
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
